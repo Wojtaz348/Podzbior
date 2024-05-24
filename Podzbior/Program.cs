8cb9cb65ef07ba346d7ef744f82202ded47d1c02
Czesław <Czesaw@DESKTOP-B3KIC2J>
2024-05-24 22:31:22 +0200

    Dodaj pliki projektów.

---
+++ Podzbior/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      
+        int[,] matrix = new int[20, 20];
+        Random random = new Random();
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                matrix[i, j] = random.Next(-99, 100); 
+            }
+        }
+
+        
+        int maxSum = int.MinValue;
+        int startRow = 0, endRow = 0;
+
+        for (int i = 0; i < 20; i++) 
+        {
+            int currentSum = 0;
+            for (int j = i; j < 20; j++) 
+            {
+                currentSum += matrix[i, j]; 
+                if (currentSum > maxSum) 
+                {
+                    maxSum = currentSum; 
+                    startRow = i;
+                    endRow = j; 
+                }
+            }
+        }
+
+        Console.WriteLine($"Największa suma podciągu: {maxSum}");
+        Console.WriteLine($"Pozycja podciągu: Rozpoczyna się na wierszu {startRow} i kończy na wierszu {endRow}");
+    }
+}
